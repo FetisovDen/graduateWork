e4f72bf2f6fe352425d386b59d01baa3563be3dc
555 <fetisovden@yandex.ru>
2023-02-20 21:04:26 +0300

    Добавил сущности, подключил бд, настроил liquibase

---
+++ src/main/java/ru/skypro/homework/entity/Avatar.java
@@ -0,0 +1,33 @@
+package ru.skypro.homework.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "avatar")
+public class Avatar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String pathAvatar;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Avatar avatar = (Avatar) o;
+        return id != null && Objects.equals(id, avatar.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/java/ru/skypro/homework/entity/User.java
@@ -0,0 +1,47 @@
+package ru.skypro.homework.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import ru.skypro.homework.dto.Role;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String city;
+    private Timestamp regDate;
+    private String userName;
+    private String password;
+    @OneToOne
+    @JoinColumn(name = "id_avatar")
+    private Avatar avatar;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
